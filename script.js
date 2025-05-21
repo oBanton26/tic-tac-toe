61ec84cb8ac5ff0f75a211a7c9d3fa5b38326d3e
Ulysse <ulysse.aubanton@gmail.com>
2025-05-21 19:13:34 +0200

    Create the console game with much pain
    I don't know why but I didn't want to draft a plan of the programm so
    it was hard to know where to start. But I managed to build the game only
    through the console. I've added the condition for the position to be
    empty, and the win condition (big bulky if). ot proud but not motivated.

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const gameboard = (function(){
-    let gameboardArray = [];
+    let gameboardArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     const addPlay = function (player, playPosition) {
-        gameboardArray.push(playPosition)
+        gameboardArray[playPosition] = player.name;
     };
     const show = function() {return gameboardArray};
-    const clean = function() {gameboardArray = []};
-    return {addPlay, show, clean};
+    const clean = function () { gameboardArray = [0, 1, 2, 3, 4, 5, 6, 7, 8]};
+    const getStateAt = (position) => {return gameboardArray[position]}
+    return {addPlay, show, clean, getStateAt};
 })();
 
 
@@ -17,6 +18,58 @@ function createPlayer (name) {
 };
 
 function createGame (player1, player2) {
-    gameboard.clean();
-    
-}
+    const players = [player1, player2];
+    let activePlayer = players[0];
+
+    const getActivePlayer = () => activePlayer;
+
+    const switchPlayerTurn = () => {
+        activePlayer = (activePlayer === players[0])? players[1] : players[0];
+    };
+
+    const playRound = (playPosition) => {
+        if (gameboard.getStateAt(playPosition) !== players[0].name && gameboard.getStateAt(playPosition) !== players[1].name) {
+            gameboard.addPlay(activePlayer, playPosition);
+
+            if (checkForWin()) {
+                activePlayer.addWin();
+                gameboard.clean();
+                console.log(`Oh my god, ${activePlayer.name} has won this round !!!!`);
+                switchPlayerTurn();
+            } else {
+                switchPlayerTurn();
+                console.log(`${activePlayer.name} is the next player to play the next play...`)
+            };
+        } else {
+            console.log("Hey that spot is taken friend");
+        }
+        
+        
+    };
+
+    const checkForWin = () => {
+        const currentBoard = gameboard.show();
+        if (currentBoard[0] === currentBoard[1] && currentBoard[1] === currentBoard[2] ||
+            currentBoard[3] === currentBoard[4] && currentBoard[4] === currentBoard[5] ||
+            currentBoard[6] === currentBoard[7] && currentBoard[7] === currentBoard[8] ||
+
+            currentBoard[0] === currentBoard[3] && currentBoard[3] === currentBoard[6] ||
+            currentBoard[1] === currentBoard[4] && currentBoard[4] === currentBoard[7] ||
+            currentBoard[2] === currentBoard[5] && currentBoard[5] === currentBoard[8] ||
+
+            currentBoard[0] === currentBoard[4] && currentBoard[4] === currentBoard[8] ||
+            currentBoard[2] === currentBoard[4] && currentBoard[4] === currentBoard[6]
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    return {getActivePlayer, switchPlayerTurn, playRound, checkForWin}
+};
+
+
+const me = createPlayer("Ulysse");
+const ai = createPlayer("AI");
+const game = createGame(me, ai);
